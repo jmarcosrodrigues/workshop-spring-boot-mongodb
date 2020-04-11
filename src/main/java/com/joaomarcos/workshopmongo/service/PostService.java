69a7fe47e29e713e2d0338d7ce2c2167734795bf
João Marcos <marcosrodrigues200@msn.com>
2020-04-11 18:07:23 -0300

    Consulta com vários critérios

--- src/main/java/com/joaomarcos/workshopmongo/service/PostService.java
+++ src/main/java/com/joaomarcos/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.joaomarcos.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 		//List<Post> posts = repository.findByTitleContainingIgnoreCase(text);
 		return repository.searchTitle(text);
 	}
+	
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+    	maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+    	return repository.fullSearch(text, minDate, maxDate);
+    }
 	
 	
 	
