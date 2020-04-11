a7ac508341dc020843396a8e6097743acc95f4d9
João Marcos <marcosrodrigues200@msn.com>
2020-04-11 16:18:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/joaomarcos/workshopmongo/service/PostService.java
@@ -0,0 +1,27 @@
+package com.joaomarcos.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaomarcos.workshopmongo.domain.Post;
+import com.joaomarcos.workshopmongo.repository.PostRepository;
+import com.joaomarcos.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+
+
+}
